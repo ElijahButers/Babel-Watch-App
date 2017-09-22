0047c3f85053b30da0d624e555efa87934d1cd08
ElijahButers <Elijahbuters@gmail.com>
2017-09-23 00:33:53 +0300

    Deleted a willActivate() and didDeactivate() methods from InterfaceController class.

--- Babel/Babel Watch Extension/InterfaceController.swift
+++ Babel/Babel Watch Extension/InterfaceController.swift
@@ -25,16 +25,4 @@ class InterfaceController: WKInterfaceController {
     }
     @IBAction func checkAnswer() {
     }
-    
-    
-    override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
-        super.willActivate()
-    }
-    
-    override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
-        super.didDeactivate()
-    }
-
 }
